4b80bb39561a43983eb5f9221bfbc953e1706dd2
SerfyWerfy <eatbatterys@protonmail.com>
2017-12-09 08:13:36 +0000

    Nomenclature

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -37,7 +37,7 @@ void SendMoney(const CTxDestination& address, CAmount nValue, CWalletTx& wtxNew,
         throw JSONRPCError(RPC_WALLET_ERROR, strError);
     }
 
-    // Parse Pivx address
+    // Parse Bulwark address
     CScript scriptPubKey = GetScriptForDestination(address);
 
     // Create and send the transaction
@@ -86,7 +86,7 @@ Value obfuscation(const Array& params, bool fHelp)
 
     CBitcoinAddress address(params[0].get_str());
     if (!address.IsValid())
-        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Pivx address");
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Bulwark address");
 
     // Amount
     CAmount nAmount = AmountFromValue(params[1]);
