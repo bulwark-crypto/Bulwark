4ad543511bee9456a718b9bdc3080c08c089df18
SerfyWerfy <SerfyWerfy@protonmail.com>
2018-02-05 10:24:19 +0000

    Increment version, protocol

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70810;
+static const int PROTOCOL_VERSION = 70811;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
