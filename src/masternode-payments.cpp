e834dd9210932e57b5882965d4b65fef06b79ae8
SerfyWerfy <SerfyWerfy@protonmail.com>
2018-02-21 16:18:44 -0500

    Remove extraneous condition

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -292,7 +292,6 @@ void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, int64_t nFe
         if (winningNode) {
             payee = GetScriptForDestination(winningNode->pubKeyCollateralAddress.GetID());
         } else {
-            LogPrintf("CreateNewBlock: Failed to detect masternode to pay\n");
             hasPayment = false;
         }
     }
@@ -326,10 +325,7 @@ void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, int64_t nFe
         CBitcoinAddress address2(address1);
 
         LogPrintf("Masternode payment of %s to %s\n", FormatMoney(masternodePayment).c_str(), address2.ToString().c_str());
-    } else {
-		if (!fProofOfStake)
-			txNew.vout[0].nValue = blockValue - masternodePayment;
-	}
+    }
 }
 
 int CMasternodePayments::GetMinMasternodePaymentsProto()
