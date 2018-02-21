33a01d5c508971de66cff56f75e2514e2f09791a
SerfyWerfy <SerfyWerfy@protonmail.com>
2018-02-21 15:24:45 -0500

    Logic update in GetBlockValue() and GetMasternodePayment() calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1625,53 +1625,53 @@ int64_t GetBlockValue(int nHeight)
     } else if (nHeight < Params().RAMP_TO_BLOCK()) {
             nSlowSubsidy /= Params().RAMP_TO_BLOCK();
             nSlowSubsidy *= nHeight;
-    } else if (nHeight <= 86400 && nHeight >= Params().RAMP_TO_BLOCK()) {
+    } else if (nHeight <= 86399 && nHeight >= Params().RAMP_TO_BLOCK()) {
 	nSubsidy = 50 * COIN;
-    } else if (nHeight <= 172800 && nHeight > 86400) {
+    } else if (nHeight <= 172799 && nHeight >= 86400) {
         nSubsidy = 43.75 * COIN;
-    } else if (nHeight <= 259200 && nHeight > 172800) {
+    } else if (nHeight <= 259199 && nHeight >= 172800) {
         nSubsidy = 37.5 * COIN;
-    } else if (nHeight <= Params().LAST_POW_BLOCK() && nHeight > 259200) {
+    } else if (nHeight <= Params().LAST_POW_BLOCK() && nHeight >= 259200) {
         nSubsidy = 31.25 * COIN;
 
     // POS Year 1
-    } else if (nHeight <= 432000 && nHeight > Params().LAST_POW_BLOCK()) {
+    } else if (nHeight <= 431999 && nHeight > Params().LAST_POW_BLOCK()) {
         nSubsidy = 25 * COIN;
-    } else if (nHeight <= 518400 && nHeight > 432000) {
+    } else if (nHeight <= 518399 && nHeight >= 432000) {
         nSubsidy = 21.875 * COIN;
-    } else if (nHeight <= 604800 && nHeight > 518400) {
+    } else if (nHeight <= 604799 && nHeight >= 518400) {
         nSubsidy = 18.750 * COIN;
-    } else if (nHeight <= 691200 && nHeight > 604800) {
+    } else if (nHeight <= 691199 && nHeight >= 604800) {
         nSubsidy = 15.625 * COIN;
 
     // POS Year 2
-    } else if (nHeight <= 777600 && nHeight > 691200) {
+    } else if (nHeight <= 777599 && nHeight >= 691200) {
         nSubsidy = 12.50 * COIN;
-    } else if (nHeight <= 864000 && nHeight > 777600) {
+    } else if (nHeight <= 863999 && nHeight >= 777600) {
         nSubsidy = 10.938 * COIN;
-    } else if (nHeight <= 950400 && nHeight > 864000) {
+    } else if (nHeight <= 950399 && nHeight >= 864000) {
         nSubsidy = 9.375 * COIN;
-    } else if (nHeight <= 1036800 && nHeight > 950400) {
+    } else if (nHeight <= 1036799 && nHeight >= 950400) {
         nSubsidy = 7.812 * COIN;
 
     // POS Year 3
-    } else if (nHeight <= 1123200 && nHeight > 1036800) {
+    } else if (nHeight <= 1123199 && nHeight >= 1036800) {
         nSubsidy = 6.250 * COIN;
-    } else if (nHeight <= 1209600 && nHeight > 1123200) {
+    } else if (nHeight <= 1209599 && nHeight >= 1123200) {
         nSubsidy = 5.469 * COIN;
-    } else if (nHeight <= 1296000 && nHeight > 1209600) {
+    } else if (nHeight <= 1295999 && nHeight >= 1209600) {
         nSubsidy = 4.688 * COIN;
-    } else if (nHeight <= 1382400 && nHeight > 1296000) {
+    } else if (nHeight <= 1382399 && nHeight >= 1296000) {
         nSubsidy = 3.906 * COIN;
 
     // POS Year 4
-    } else if (nHeight <= 1468800 && nHeight > 1382400) {
+    } else if (nHeight <= 1468799 && nHeight >= 1382400) {
         nSubsidy = 3.125 * COIN;
-    } else if (nHeight <= 1555200 && nHeight > 1468800) {
+    } else if (nHeight <= 1555199 && nHeight >= 1468800) {
         nSubsidy = 2.734 * COIN;
-    } else if (nHeight <= 1641600 && nHeight > 1555200) {
+    } else if (nHeight <= 1641599 && nHeight >= 1555200) {
         nSubsidy = 2.344 * COIN;
-    } else if (nHeight <= 1728000 && nHeight > 1641600) {
+    } else if (nHeight <= 1727999 && nHeight >= 1641600) {
         nSubsidy = 1.953 * COIN;
 
     } else if (nHeight > 1728000) {
