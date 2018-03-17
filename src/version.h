3e5d54153929342c8d7c3c63a1f5deef47af9964
opensourcerulez <krugliak@yandex.ru>
2018-03-18 00:03:07 +0300

    add 2 protos

--- src/version.h
+++ src/version.h
@@ -21,8 +21,10 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70820;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70830;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT15 = 70811;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT15 = 70820;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT17 = 70820;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT17 = 70830;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
