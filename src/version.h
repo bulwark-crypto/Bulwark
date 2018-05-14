82845caa152336c1dc3ca9c432838b555b140576
dustinengle <dustin.engle@gmail.com>
2018-05-14 15:43:25 -0700

    updated protocol version

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70820;
+static const int PROTOCOL_VERSION = 70830;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -24,6 +24,7 @@ static const int GETHEADERS_VERSION = 70077;
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70820;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT15 = 70820;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT17 = 70830;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT18 = 70840;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
