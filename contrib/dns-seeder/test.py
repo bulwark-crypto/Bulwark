659b8eca9fbad5ec5b12197b5cd440271e78b7c1
masterhash-us <32805608+masterhash-us@users.noreply.github.com>
2018-06-11 07:59:51 +1000

    Added New Seeders to Test

--- contrib/dns-seeder/test.py
+++ contrib/dns-seeder/test.py
@@ -2,6 +2,16 @@ import socket
 
 seeders = [
     'bwkseed.mempool.pw',
+    'bwkseed1.bulwarkcrypto.com',
+    'bwkseed2.bulwarkcrypto.com',
+    'bwkseed3.bulwarkcrypto.com',
+    'bwkseed4.bulwarkcrypto.com',
+    'bwkseed5.bulwarkcrypto.com',
+    'bwkseed1.bulwarkcrypto.site',
+    'bwkseed2.bulwarkcrypto.site',
+    'bwkseed3.bulwarkcrypto.site',
+    'bwkseed4.bulwarkcrypto.site',
+    'bwkseed5.bulwarkcrypto.site',
     'bwk.goseed.club'
 ]
 
