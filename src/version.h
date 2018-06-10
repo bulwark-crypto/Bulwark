2361b7f1066275e2880c0d57ccb94757be46fce8
dustinengle <dustin.engle@gmail.com>
2018-06-10 11:48:42 -0700

    updated protocol versions for spork to match current deployment

--- src/version.h
+++ src/version.h
@@ -22,9 +22,9 @@ static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70820; // [stu] TODO: verify correct protocol handling.
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT15 = 70830;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT17 = 70840;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT18 = 70850;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT15 = 70820;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT17 = 70830;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT18 = 70840;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
