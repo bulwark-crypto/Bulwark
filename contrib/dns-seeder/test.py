67abc360c0a4a3767119c1afd80e9a3388024195
dustinengle <dustin.engle@gmail.com>
2018-06-10 17:57:37 -0700

    added duplicate address prevention to dns seeder test count for now

--- contrib/dns-seeder/test.py
+++ contrib/dns-seeder/test.py
@@ -20,4 +20,12 @@ for seeder in seeders:
     except socket.gaierror:
         ais = []
     
-    print(seeder + ' = ' + str(len(ais)))
+    # Prevent duplicates, need to update to check
+    # for ports, can have multiple nodes on 1 ip.
+    addrs = []
+    for a in ais:
+        addr = a[4][0]
+        if addrs.count(addr) == 0:
+            addrs.append(addr)
+    
+    print(seeder + ' = ' + str(len(addrs)))
