3797cd8d9df80908259d95c08dcb2d78165e8720
meiskam <meiskam@gmail.com>
2018-09-18 04:41:14 -0400

    Revert "fixed more spots where zeromint was still 10%", causes crash on load
    This reverts commit 0e85bf02ae08e76ba1253c8d6b36dcacf73db03e.

--- src/util.cpp
+++ src/util.cpp
@@ -115,8 +115,8 @@ bool fLiteMode = false;
 bool fEnableSwiftTX = true;
 int nSwiftTXDepth = 5;
 // Automatic Zerocoin minting
-bool fEnableZeromint = false;
-int nZeromintPercentage = 0;
+bool fEnableZeromint = true;
+int nZeromintPercentage = 10;
 int nPreferredDenom = 0;
 const int64_t AUTOMINT_DELAY = (60 * 5); // Wait at least 5 minutes until Automint starts
 
