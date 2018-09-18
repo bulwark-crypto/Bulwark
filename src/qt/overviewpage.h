61a60c436bf49aa853f321b09c759d3e6d214dd7
dustinengle <dustin.engle@gmail.com>
2018-09-18 14:25:39 -0700

    fixed error, remove toggle staking button slot

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -40,7 +40,6 @@ public slots:
     void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance, 
                     const CAmount& zerocoinBalance, const CAmount& unconfirmedZerocoinBalance, const CAmount& immatureZerocoinBalance,
                     const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance);
-	void on_toggleStaking_clicked();
 
 signals:
     void transactionClicked(const QModelIndex& index);
