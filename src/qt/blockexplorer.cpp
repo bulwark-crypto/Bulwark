cb1438c491769d30f3217748061198b58780601b
dustinengle <dustin.engle@gmail.com>
2018-09-19 10:17:34 -0700

    updated block explorer style

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -550,8 +550,8 @@ void BlockExplorer::setBlock(CBlockIndex* pBlock)
 }
 
 void BlockExplorer::setContent(const std::string& Content)
-{
-    QString CSS = "body {font-size:12px; color:#f8f6f6; bgcolor:#5B4C7C;}\n a, span { font-family: monospace; }\n span.addr {color:#5B4C7C; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #5B4C7C;}\n td.d0 {font-weight: bold; color:#f8f6f6;}\n h2, h3 { white-space:nowrap; color:#5B4C7C;}\n a { color:#88f6f6; text-decoration:none; }\n a.nav {color:#5B4C7C;}\n";
+{ 
+    QString CSS = "body {font-size:12px; color:#f8f6f6; bgcolor:#0091ea;}\n a, span { font-family: monospace; }\n span.addr {color:#0091ea; font-weight: bold;}\n table tr td {padding: 3px; border: 1px solid black; background-color: #0091ea;}\n td.d0 {font-weight: bold; color:#f8f6f6;}\n h2, h3 { white-space:nowrap; color:#0091ea;}\n a { color:#ffffff; text-decoration:none; }\n a:hover { color:#cccccc; }\n a.nav {color:#0091ea;}\n";
     QString FullContent = "<html><head><style type=\"text/css\">" + CSS + "</style></head>" + "<body>" + Content.c_str() + "</body></html>";
     // printf(FullContent.toUtf8());
 
