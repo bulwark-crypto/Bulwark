1a508f3f574c10c2cacfe84b8a833b4cde15ccab
Kolby Moroz <31669092+mrmetech@users.noreply.github.com>
2018-11-21 12:46:09 -0700

    Update configuremasternodepage.cpp

--- src/qt/configuremasternodepage.cpp
+++ src/qt/configuremasternodepage.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/curium-config.h"
+#include "config/phore-config.h"
 #endif
 
 #include "configuremasternodepage.h"
