8fb88638a5579c1f2361c3654436746e607d8514
Akshay <akshaycm@hotmail.com>
2019-09-19 17:49:55 +0300

    remove redefinition

--- src/hash.h
+++ src/hash.h
@@ -31,35 +31,6 @@ using namespace std;
 
 typedef uint256 ChainCode;
 
-/** A hasher class for Bitcoin's 256-bit hash (double SHA-256). */
-class CHash256
-{
-private:
-    CSHA256 sha;
-
-public:
-    static const size_t OUTPUT_SIZE = CSHA256::OUTPUT_SIZE;
-
-    void Finalize(unsigned char hash[OUTPUT_SIZE])
-    {
-        unsigned char buf[CSHA256::OUTPUT_SIZE];
-        sha.Finalize(buf);
-        sha.Reset().Write(buf, CSHA256::OUTPUT_SIZE).Finalize(hash);
-    }
-
-    CHash256& Write(const unsigned char* data, size_t len)
-    {
-        sha.Write(data, len);
-        return *this;
-    }
-
-    CHash256& Reset()
-    {
-        sha.Reset();
-        return *this;
-    }
-};
-
 class CHash512
 {
 private:
