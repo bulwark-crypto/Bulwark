fddae12cc845f75d21c67179c03fae2744b22333
Kolby Moroz <mrmetech@gmail.com>
2019-12-14 22:51:22 -0700

    Files weren't created in 2017 they were created in 2018

--- src/qt/proposalfilterproxy.h
+++ src/qt/proposalfilterproxy.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2018 The Phore developers
 // Copyright (c) 2018 The Curium developers
-// Copyright (c) 2017-2019 The Bulwark Developers
+// Copyright (c) 2018-2019 The Bulwark Developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
